a7a68a02745e59a6f856ad470fda8dec1fa2cf1d
mazdamike <mwpowell44@gmail.com>
2020-03-21 18:14:36 -0400

    saving the final password generator app to turn in

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # week3PasswordGenerator
 Week 3 homework Password Generator
+
+Asks the user how long the user wants the password to be; then ask the user what types of characters the user wants to be in the password; then generate a random password of that length using the chosen character types.
--- script.js
+++ script.js
@@ -12,17 +12,11 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Generates the password
+// Start the main function to generate the password
 function generatePassword() {
 
-  // Find out how long the user wants the password to be
-  var numCharacters = prompt("How long would you like your password to be (must be between 8-128 characters)?");
-  numCharacters = parseInt(numCharacters);
-
-  if (numCharacters < 8 || numCharacters > 128) {
-    alert("You must enter a number between 8 and 128.");
-    generatePassword();
-  }
+  // Find out how long the user wants the password to be  
+  var numCharacters = lengthChecker();
 
   // Get the full character set that will be used to generate the password
   var fullCharSet = getCharSet();
@@ -37,14 +31,26 @@ function generatePassword() {
   return retnVal;
 }
 
-// Get the character set by asking the user what types of characters he wants to include
-function getCharSet() {
+// Function to get the length the user wants the password to be (note: recursive function)
+function lengthChecker() {
+  var numChosen = prompt("How long would you like your password to be (must be between 8-128 characters)?");
+  numChosen = parseInt(numChosen);
 
+  if (numChosen < 8 || numChosen > 128) {
+    alert("You must enter a number between 8 and 128.");
+    return lengthChecker();
+  } else {
+    return numChosen;
+  }
+}
+
+// Function to get the character set by asking the user what types of characters the user wants to include (note: recursive function)
+function getCharSet() {
   var lowerCase = "abcdefghijklmnopqrstuvwxyz";
   var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numerals = "0123456789";
   var specialChars = "~@#$%^&*()_-+=<>";
-  var charSet = ""
+  var charSet = "";
 
   var includeLower = confirm("Do you want to include lowercase letters?");
   if (includeLower) {
@@ -68,9 +74,8 @@ function getCharSet() {
 
   if (!includeLower && !includeUpper && !includeNumbers && !includeSpecial) {
     alert("You must choose at least one type of character.");
-    getCharSet();
+    return getCharSet();
   } else {
-    console.log(charSet);
     return charSet;
   }
 }

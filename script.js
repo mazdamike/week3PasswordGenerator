1d2584219f7cbd1197d7e94d1eb08172adff3c36
mazdamike <mwpowell44@gmail.com>
2020-03-20 22:17:11 -0400

    saving homework Friday night.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
+
   passwordText.value = password;
 }
 
@@ -14,29 +14,30 @@ generateBtn.addEventListener("click", writePassword);
 
 // Generates the password
 function generatePassword() {
-  
+
   // Find out how long the user wants the password to be
   var numCharacters = prompt("How long would you like your password to be (must be between 8-128 characters)?");
   numCharacters = parseInt(numCharacters);
-  
+
   if (numCharacters < 8 || numCharacters > 128) {
     alert("You must enter a number between 8 and 128.");
     generatePassword();
-  } 
+  }
 
   // Get the full character set that will be used to generate the password
   var fullCharSet = getCharSet();
-  
 
-}  
-  
-  
-  
-  
-  
-  
-  
-  // Get the character set by asking the user what types of characters he wants to include
+  // Generate and return the password
+  var lenOfSet = fullCharSet.length;
+  var retnVal = "";
+
+  for (var i = 0; i < numCharacters; i++) {
+    retnVal += fullCharSet.charAt(Math.floor(Math.random() * lenOfSet));
+  }
+  return retnVal;
+}
+
+// Get the character set by asking the user what types of characters he wants to include
 function getCharSet() {
 
   var lowerCase = "abcdefghijklmnopqrstuvwxyz";
@@ -44,7 +45,7 @@ function getCharSet() {
   var numerals = "0123456789";
   var specialChars = "~@#$%^&*()_-+=<>";
   var charSet = ""
-  
+
   var includeLower = confirm("Do you want to include lowercase letters?");
   if (includeLower) {
     charSet = charSet.concat(lowerCase);
@@ -54,20 +55,22 @@ function getCharSet() {
   if (includeUpper) {
     charSet = charSet.concat(upperCase);
   }
-  
+
   var includeNumbers = confirm("Do you want to include numbers?");
   if (includeNumbers) {
     charSet = charSet.concat(numerals);
   }
-  
+
   var includeSpecial = confirm("Do you want to include special characters?");
   if (includeSpecial) {
     charSet = charSet.concat(specialChars);
   }
-  
+
   if (!includeLower && !includeUpper && !includeNumbers && !includeSpecial) {
     alert("You must choose at least one type of character.");
-    getCharSet()
+    getCharSet();
+  } else {
+    console.log(charSet);
+    return charSet;
   }
-  return charSet;
 }
